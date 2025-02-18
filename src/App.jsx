113add29727245cd28b4de6af4de1673058e3fd8
UsielOM <usiel31197@outlook.es>
2025-02-18 00:13:45 -0600

    feat(*):Creacion de pagina de bienvenida y eliminacion de estilos por defecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import MxxmeNavBar from  './components/NavBar/MxxmeNavBar.jsx';
+import Onboarding from './page/Onboarding/Onboarding.jsx';
 
 function App() {
 
 
   return (
     <>
-    <MxxmeNavBar></MxxmeNavBar>
+        <MxxmeNavBar></MxxmeNavBar>
+        <Onboarding ></Onboarding>
     </>
   )
 }
